677a7d6744a057ed3384a0fc8da2c62a11e8d0ee
ryuji <ryujispjp@gmail.com>
2021-11-22 01:58:23 +0900

    first commit

---
+++ iOSPractice/TableView/TableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewController.swift
+//  iOSPractice
+//
+//  Created by 髙橋　竜治 on 2021/11/22.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
