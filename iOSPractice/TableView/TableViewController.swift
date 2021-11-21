46591aeac7a7bd27d7f1bca90a3b9c117e97ac9b
ryuji <ryujispjp@gmail.com>
2021-11-22 02:32:14 +0900

    とりあえずtableView実装 後で理解する

--- iOSPractice/TableView/TableViewController.swift
+++ iOSPractice/TableView/TableViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class TableViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
+
+    private let items: [String] = ["A", "B", "C","D", "1", "2", "3", "4"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TableView.dataSource = self
+    }
+}
+
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ??
+        UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 }
