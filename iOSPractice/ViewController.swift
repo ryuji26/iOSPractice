571578b1394e85f161bcc6ec6411edb5789dbf2a
ryuji <ryujispjp@gmail.com>
2021-11-22 01:51:52 +0900

    Initial Commit

---
+++ iOSPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPractice
+//
+//  Created by 髙橋　竜治 on 2021/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
