7d0bd649d1057f7fdd74a127826670b828f74266
ryuji <ryujispjp@gmail.com>
2021-11-26 17:47:20 +0900

    CollectionView追加

---
+++ iOSPractice/CollectionView/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  iOSPractice
+//
+//  Created by 髙橋　竜治 on 2021/11/26.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ iOSPractice/CollectionView/CustomCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewController.swift
+//  iOSPractice
+//
+//  Created by 髙橋　竜治 on 2021/11/26.
+//
+
+import UIKit
+
+class CustomCollectionViewController: UICollectionViewController {
+    
+}
